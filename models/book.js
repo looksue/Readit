724f08e0b7653f8f90a84bba0fd9b88157d0c446
Sue Look <suelook1@gmail.com>
2019-10-30 19:55:52 -0400

    updated index handlebars

--- models/book.js
+++ models/book.js
@@ -1,11 +1,17 @@
 module.exports = function(sequelize, DataTypes) {
-  var Book = sequelize.define("Book", {
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
-    description: DataTypes.STRING,
-    genre: DataTypes.STRING,
-    rating: DataTypes.STRING,
-    cover: DataTypes.STRING
-  });
+  var Book = sequelize.define(
+    "Book",
+    {
+      title: DataTypes.STRING,
+      author: DataTypes.STRING,
+      summary: DataTypes.STRING,
+      genre: DataTypes.STRING,
+      rating: DataTypes.STRING,
+      cover: DataTypes.STRING
+    },
+    {
+      timestamps: false
+    }
+  );
   return Book;
 };
